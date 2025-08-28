263cc56eb677b9609c6c344ed6d0c8b7bfaecd77
kakul-gautam <gautamkakul5@gmail.com>
2025-08-28 21:18:35 +0530

    [kakul]odd even checker

---
+++ ControlFlow/level1/level2/OddEvenChecker.java
@@ -0,0 +1,22 @@
+package ControlFlow.level1.level2;
+
+import java.util.Scanner;
+
+public class OddEvenChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a natural number (positive integer): ");
+        int number = scanner.nextInt();
+        if (number <= 0) {
+            System.out.println("Invalid input. Please enter a positive integer.");
+            return;
+        }
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is an Even number");
+            } else {
+                System.out.println(i + " is an Odd number");
+            }
+        }
+    }
+}
