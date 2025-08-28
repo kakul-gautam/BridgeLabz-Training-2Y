0b38a03b808db60a7cd2ce1185eac331e7086bbf
kakul-gautam <gautamkakul5@gmail.com>
2025-08-28 21:32:27 +0530

    [kakul]fiz buzz while loop

---
+++ ControlFlow/level1/level2/FizzBuzzWhileLoop.java
@@ -0,0 +1,31 @@
+package ControlFlow.level1.level2;
+import java.util.Scanner;
+
+public class FizzBuzzWhileLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+        if (number <= 0) {
+            System.out.println("Invalid input. Please enter a positive integer.");
+            return;
+        }
+        int i = 1;
+        System.out.println("FizzBuzz results from 1 to " + number + ":");
+        while (i <= number) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
+
+    
+
