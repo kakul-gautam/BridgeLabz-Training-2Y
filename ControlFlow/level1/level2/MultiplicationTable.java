c2a1b7ede672e19651a8795e38361831ce2e40a2
kakul-gautam <gautamkakul5@gmail.com>
2025-08-28 21:27:56 +0530

    [kakul]multiplication table

---
+++ ControlFlow/level1/level2/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package ControlFlow.level1.level2;
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to generate its multiplication table (from 6 to 9): ");
+        int number = scanner.nextInt();
+        System.out.println("Multiplication table of " + number + " from 6 to 9:");
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+    }
+}
