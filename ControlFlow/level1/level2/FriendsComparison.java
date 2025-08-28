80edc26fa8e99a93b698a0787534a0c605c9bfd8
kakul-gautam <gautamkakul5@gmail.com>
2025-08-28 21:35:47 +0530

    [kakul]friends comparision

---
+++ ControlFlow/level1/level2/FriendsComparison.java
@@ -0,0 +1,46 @@
+package ControlFlow.level1.level2;
+import java.util.*;
+
+public class FriendsComparison {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Amar's age: ");
+        int ageAmar = scanner.nextInt();
+        System.out.print("Enter Akbar's age: ");
+        int ageAkbar = scanner.nextInt();
+        System.out.print("Enter Anthony's age: ");
+        int ageAnthony = scanner.nextInt();
+        System.out.print("Enter Amar's height (in cm): ");
+        int heightAmar = scanner.nextInt();
+        System.out.print("Enter Akbar's height (in cm): ");
+        int heightAkbar = scanner.nextInt();
+        System.out.print("Enter Anthony's height (in cm): ");
+        int heightAnthony = scanner.nextInt();
+        String youngest;
+        int minAge = ageAmar;
+        youngest = "Amar";
+
+        if (ageAkbar < minAge) {
+            minAge = ageAkbar;
+            youngest = "Akbar";
+        }
+        if (ageAnthony < minAge) {
+            minAge = ageAnthony;
+            youngest = "Anthony";
+        }
+        String tallest;
+        int maxHeight = heightAmar;
+        tallest = "Amar";
+
+        if (heightAkbar > maxHeight) {
+            maxHeight = heightAkbar;
+            tallest = "Akbar";
+        }
+        if (heightAnthony > maxHeight) {
+            maxHeight = heightAnthony;
+            tallest = "Anthony";
+        }
+        System.out.println("\nThe youngest friend is: " + youngest + " (Age: " + minAge + ")");
+        System.out.println("The tallest friend is: " + tallest + " (Height: " + maxHeight + " cm)");
+    }
+}
