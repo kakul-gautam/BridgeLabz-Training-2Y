28e61d4cb851b8f70b0841d9fe0e21850254b846
kakul-gautam <gautamkakul5@gmail.com>
2025-08-28 12:38:51 +0530

    [kakul]countdown

---
+++ ControlFlow/level1/RocketLaunchCountdown.java
@@ -0,0 +1,17 @@
+package ControlFlow.level1;
+import java.util.Scanner;
+
+public class RocketLaunchCountdown {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter countdown start value: ");
+        int counter = scanner.nextInt();
+        while (counter >= 1) {
+            System.out.println("Countdown: " + counter);
+            counter--;
+        }
+        System.out.println("🚀 Launch!");
+
+        scanner.close();
+    }
+}
