06e6d018c2b18e8b02d2acad0e048a3914fa9c93
kakul-gautam <gautamkakul5@gmail.com>
2025-08-28 12:54:47 +0530

    [kakul]sum until 0

--- ControlFlow/level1/RocketLaunchCountdownForLoop.java
+++ ControlFlow/level1/RocketLaunchCountdownForLoop.java
@@ -4,17 +4,11 @@ import java.util.Scanner;
 public class RocketLaunchCountdownForLoop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Input: countdown starting value
         System.out.print("Enter countdown start value: ");
         int counter = scanner.nextInt();
-
-        // Countdown using for-loop
         for (int i = counter; i >= 1; i--) {
             System.out.println("Countdown: " + i);
         }
-
-        // Final launch message
         System.out.println("🚀 Launch!");
 
         scanner.close();
