a6f972d1c72b8ded8534a56a1f54b930193f34d4
kakul-gautam <gautamkakul5@gmail.com>
2025-08-28 21:45:19 +0530

    [kakul]greatest factor while loop

---
+++ ControlFlow/level1/level2/GreatestFactorWhileLoop.java
@@ -0,0 +1,25 @@
+package ControlFlow.level1.level2;
+import java.util.Scanner;
+
+public class GreatestFactorWhileLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer greater than 1: ");
+        int number = scanner.nextInt();
+        if (number <= 1) {
+            System.out.println("Please enter an integer greater than 1.");
+            return;
+        }
+        int greatestFactor = 1;
+        int counter = number - 1;
+
+        while (counter >= 1) {
+            if (number % counter == 0) {
+                greatestFactor = counter;
+                break; 
+            }
+            counter--;
+        }
+        System.out.println("The greatest factor of " + number + " (excluding itself) is: " + greatestFactor);
+    }
+}
