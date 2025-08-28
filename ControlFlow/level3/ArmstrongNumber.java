2ff9866da1096a37423501fae5edaf69058fdc47
kakul-gautam <gautamkakul5@gmail.com>
2025-08-28 22:14:34 +0530

    [kakul]armstrong number

---
+++ ControlFlow/level3/ArmstrongNumber.java
@@ -0,0 +1,22 @@
+package ControlFlow.level3;
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+        int originalNumber = number; 
+        int sum = 0;
+        while (originalNumber != 0) {
+            int digit = originalNumber % 10;   
+            sum += digit * digit * digit;           
+            originalNumber = originalNumber / 10;  
+        }
+        if (sum == number) {
+            System.out.println(number + " is an Armstrong Number.");
+        } else {
+            System.out.println(number + " is NOT an Armstrong Number.");
+        }
+    }
+}
