8dbd6690d68437c6341f604ddc48980d292d2538
kakul-gautam <gautamkakul5@gmail.com>
2025-08-29 11:02:03 +0530

    [kakul]factor finder

---
+++ Arrays/level1/FactorFinder.java
@@ -0,0 +1,38 @@
+package Arrays.level1;
+import java.util.Scanner;
+
+public class FactorFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer to find its factors: ");
+        int number = scanner.nextInt();
+
+        if (number <= 0) {
+            System.out.println("Error: Please enter a positive integer.");
+            return;
+        }
+        int maxFactor = 10;
+        int[] factors = new int[maxFactor];
+        int index = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                if (index == maxFactor) {
+                    maxFactor *= 2;
+                    int[] temp = new int[maxFactor];
+                    for (int j = 0; j < factors.length; j++) {
+                        temp[j] = factors[j];
+                    }
+                    factors = temp;
+                }
+                factors[index] = i;
+                index++;
+            }
+        }
+        System.out.println("\nFactors of " + number + ":");
+        for (int i = 0; i < index; i++) {
+            System.out.print(factors[i] + " ");
+        }
+
+        scanner.close();
+    }
+}
