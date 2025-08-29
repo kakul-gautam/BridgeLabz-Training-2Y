e97ebd4e02913f930266b451f3095b0fb42c81c8
kakul-gautam <gautamkakul5@gmail.com>
2025-08-29 12:42:50 +0530

    [kakul]digit frequency

---
+++ Arrays/level2/DigitFrequency.java
@@ -0,0 +1,41 @@
+package Arrays.level2;
+import java.util.Scanner;
+
+public class DigitFrequency {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+
+        if (number < 0) {
+            System.out.println("Error: Please enter a non-negative integer.");
+            return;
+        }
+        int temp = number;
+        int count = 0;
+        while (temp != 0) {
+            count++;
+            temp /= 10;
+        }
+
+        int[] digits = new int[count];
+        temp = number;
+        for (int i = 0; i < count; i++) {
+            digits[i] = temp % 10;
+            temp /= 10;
+        }
+        int[] frequency = new int[10]; 
+
+        for (int i = 0; i < count; i++) {
+            frequency[digits[i]]++;
+        }
+        System.out.println("\nDigit Frequency:");
+        for (int i = 0; i < 10; i++) {
+            if (frequency[i] > 0) {
+                System.out.println("Digit " + i + " occurs " + frequency[i] + " time(s)");
+            }
+        }
+
+        scanner.close();
+    }
+}
