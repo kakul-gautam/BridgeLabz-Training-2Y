cb2418127c219088b92058c95c2fe6d2e3aa8d3d
kakul-gautam <gautamkakul5@gmail.com>
2025-08-29 10:35:31 +0530

    [kakul]multiplication table

---
+++ Arrays/level1/MultiplicationTable.java
@@ -0,0 +1,20 @@
+package Arrays.level1;
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to print its multiplication table: ");
+        int number = scanner.nextInt();
+        int[] table = new int[10];
+        for (int i = 0; i < table.length; i++) {
+            table[i] = number * (i + 1);
+        }
+        System.out.println("\nMultiplication Table of " + number + ":");
+        for (int i = 0; i < table.length; i++) {
+            System.out.println(number + " * " + (i + 1) + " = " + table[i]);
+        }
+
+        scanner.close();
+    }
+}
