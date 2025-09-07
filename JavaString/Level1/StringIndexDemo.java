3a3ee9c2b755f1c4b1239d27cb13c9bd8a741579
kakul-gautam <gautamkakul5@gmail.com>
2025-09-07 22:48:01 +0530

    [kakul]

---
+++ JavaString/Level1/StringIndexDemo.java
@@ -0,0 +1,23 @@
+package JavaString.Level1;
+
+import java.util.Scanner;
+
+public class StringIndexDemo {
+    public static void generateException(String text) {
+        System.out.println("Character at index 100: " + text.charAt(100));
+    }
+    public static void demonstrateExceptionHandling(String text) {
+        try {
+            System.out.println("Character at index 100: " + text.charAt(100));
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Caught StringIndexOutOfBoundsException: Index is out of range.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String userInput = scanner.nextLine();
+        demonstrateExceptionHandling(userInput);
+    }
+}
