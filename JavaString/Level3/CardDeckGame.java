5503ade12fbb163b810542b12546f0110f600cb0
kakul-gautam <gautamkakul5@gmail.com>
2025-09-07 23:38:47 +0530

    [kakul]

---
+++ JavaString/Level3/CardDeckGame.java
@@ -0,0 +1,75 @@
+package JavaString.Level3;
+
+import java.util.Scanner;
+
+public class CardDeckGame {
+    public static String[] initializeDeck() {
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10",
+                          "Jack", "Queen", "King", "Ace"};
+
+        int numOfCards = suits.length * ranks.length;
+        String[] deck = new String[numOfCards];
+
+        int index = 0;
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                deck[index++] = rank + " of " + suit;
+            }
+        }
+        return deck;
+    }
+    public static String[] shuffleDeck(String[] deck) {
+        int n = deck.length;
+        for (int i = 0; i < n; i++) {
+            int randomCardNumber = i + (int)(Math.random() * (n - i));
+            String temp = deck[i];
+            deck[i] = deck[randomCardNumber];
+            deck[randomCardNumber] = temp;
+        }
+        return deck;
+    }
+    public static String[][] distributeCards(String[] deck, int numCards, int numPlayers) {
+        if (numCards > deck.length || numCards % numPlayers != 0) {
+            System.out.println("Cannot distribute " + numCards + " cards evenly among " + numPlayers + " players.");
+            return null;
+        }
+
+        int cardsPerPlayer = numCards / numPlayers;
+        String[][] players = new String[numPlayers][cardsPerPlayer];
+
+        int index = 0;
+        for (int i = 0; i < numPlayers; i++) {
+            for (int j = 0; j < cardsPerPlayer; j++) {
+                players[i][j] = deck[index++];
+            }
+        }
+        return players;
+    }
+    public static void printPlayerCards(String[][] players) {
+        if (players == null) return;
+
+        for (int i = 0; i < players.length; i++) {
+            System.out.println("Player " + (i + 1) + ":");
+            for (String card : players[i]) {
+                System.out.println("  " + card);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of cards to distribute: ");
+        int numCards = scanner.nextInt();
+
+        System.out.print("Enter number of players: ");
+        int numPlayers = scanner.nextInt();
+
+        String[] deck = initializeDeck();
+        deck = shuffleDeck(deck);
+        String[][] players = distributeCards(deck, numCards, numPlayers);
+        printPlayerCards(players);
+    }
+}
